a2b06c85e21b079f611d4b1873679820590b9eae
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-06-21 16:51:22 +0200

    Creata logica di base la censura della parole

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php   
+    $paragraph = 'Beh, Shinji, io non posso fare altro che stare qui ad annaffiare. Però, quanto a te, quanto a quel che non puoi far che tu, per te qualcosa da poter far dovrebbe esserci. Ma non ti costringerà nessuno, pensa da te stesso, decidi da te stesso che cosa tu stesso possa fare. Beh, che tu non abbia rammarichi';
+
+    $word= $_GET["word"];
+
+    $newParagraph = str_replace("$word", "***", $paragraph);
+
+?>
+
+<!-- Document html -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +17,13 @@
     <title>PHP Badwords</title>
 </head>
 <body>
-        
+    <!-- parte con il paragrafo originale -->
+    <h2>Paragrafo Originale</h2>
+    <p> <?php echo $paragraph  ?> </p>
+    <p>Lunghezza del paragrafo: <?php echo strlen($paragraph) ?></p>
+    <!-- parte con il paragrafo censurato -->
+    <h2>Paragrafo Censurato</h2>
+    <p><?php echo $newParagraph ?></p>
+    <p>Lunghezza del paragrafo: <?php echo strlen($newParagraph) ?></p>
 </body>
 </html>
